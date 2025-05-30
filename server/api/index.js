81a8631b18b715fc121ce7e89b29ec5231014357
Rafif Ramdhana <rafiframadhana.cs@gmail.com>
2025-05-30 09:42:49 +0700

    update cors origin

--- server/api/index.js
+++ server/api/index.js
@@ -14,7 +14,7 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: process.env.CORS_ORIGIN,
+  origin: process.env.CORS_ORIGIN || "http://localhost:5173",
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
