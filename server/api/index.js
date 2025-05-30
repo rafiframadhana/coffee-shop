9d90afa9978054bee09c89290cd0d904664643dd
Rafif Ramdhana <rafiframadhana.cs@gmail.com>
2025-05-30 18:29:49 +0700

    fix server 7

--- server/api/index.js
+++ server/api/index.js
@@ -13,11 +13,11 @@ import routes from "../src/routes/index.js";
 const app = express();
 connectDB();
 
-app.use(cookieParser(process.env.SESSION_SECRET));
+app.use(cookieParser());
 
 app.use(
   cors({
-    origin: process.env.CORS_ORIGIN || "http://localhost:5173",
+    origin: [process.env.CORS_ORIGIN, "http://localhost:5173"],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
@@ -36,8 +36,6 @@ app.use(
       maxAge: 1000 * 60 * 60 * 24 * 7, // 1 week
       sameSite: process.env.NODE_ENV === "production" ? "None" : "Lax",
       secure: process.env.NODE_ENV === "production",
-      domain:
-        process.env.NODE_ENV === "production" ? ".vercel.app" : "localhost",
     },
     store: MongoStore.create({
       client: mongoose.connection.getClient(),
